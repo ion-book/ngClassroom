a19592fcae01276c6c227ccfcf81831bb14ccd3d
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-09-12 16:43:59 -0500

    conectando todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { FeedProvider } from '../providers/feed/feed';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+export const firebaseConfig = {
+  apiKey: "xxxxxxxxxx",
+  authDomain: "your-domain-name.firebaseapp.com",
+  databaseURL: "https://your-domain-name.firebaseio.com",
+  storageBucket: "your-domain-name.appspot.com",
+  messagingSenderId: '<your-messaging-sender-id>'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +29,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +42,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FeedProvider,
+    InAppBrowser
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavParams, IonicPage } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { FeedProvider, FeedItem, Feed } from '../../providers/feed/feed';
+ 
+@IonicPage({
+})
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  articles: FeedItem[];
+  selectedFeed: Feed;
+  loading: Boolean;
+ 
+  constructor(
+    private iab: InAppBrowser, 
+    private feedProvider: FeedProvider,
+    public navParams: NavParams) {
+    this.selectedFeed = {
+      title: "Ng-Classroom",
+      url: "https://www.ion-book.com/feed.xml"
+  };
   }
-
+ 
+  public openArticle(url: string) {
+    this.iab.create(url, '_blank');
+    // window.open(url, '_blank');
+  }
+ 
+  loadArticles() {
+    this.loading = true;
+    this.feedProvider.getArticlesForUrl(this.selectedFeed.url).subscribe(res => {
+      this.articles = res;
+      this.loading = false;
+    });
+  }
+ 
+  public ionViewWillEnter() {
+    if (this.selectedFeed !== undefined && this.selectedFeed !== null ) {
+      this.loadArticles()
+    } else {
+      this.feedProvider.getSavedFeeds().then(
+        feeds => {
+          if (feeds.length > 0) {
+            let item = feeds[0];
+            this.selectedFeed = new Feed(item.title, item.url);
+            this.loadArticles();
+          }
+        }
+      );
+    }
+  }
 }
